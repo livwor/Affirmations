378dc212a11859fa1ca1c5004feb763ec525dc4d
Livia <139778899+livwor@users.noreply.github.com>
2023-09-21 11:26:30 +0200

    new Affirmations app version with GridLayout

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.affirmations
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.affirmations.adapter.ItemAdapter
 import com.example.affirmations.data.Datasource
@@ -16,6 +17,8 @@ class MainActivity : Activity() {
 
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.adapter = ItemAdapter(this, myDataSet)
+
+        recyclerView.layoutManager = GridLayoutManager(this,3)
 
         // We can use this setting to improve performance if the layout size of the RecyclerView do not change
         recyclerView.setHasFixedSize(true)
