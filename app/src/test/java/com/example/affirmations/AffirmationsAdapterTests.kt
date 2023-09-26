e0cca3d2a1c4346f25cc0d83807b12513dae8b25
Livia <139778899+livwor@users.noreply.github.com>
2023-09-26 11:34:26 +0200

    Test the Adapter to make sure that the size of the adapter is the size of the list that was passed to the adapter, add a dependency for local tests: testImplementation 'org.mockito:mockito-core:3.12.4'

---
+++ app/src/test/java/com/example/affirmations/AffirmationsAdapterTests.kt
@@ -0,0 +1,24 @@
+package com.example.affirmations
+
+import android.content.Context
+import com.example.affirmations.adapter.ItemAdapter
+import com.example.affirmations.model.Affirmation
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.mockito.Mockito.mock
+
+class AffirmationsAdapterTests {
+    // Use the mock method to create a "mocked" instance of a Context
+    private val context = mock(Context::class.java)
+
+    @Test
+    fun adaptersize() {
+        val data = listOf(
+            Affirmation(R.string.affirmation1, R.drawable.image1),
+            Affirmation(R.string.affirmation2, R.drawable.image2)
+        )
+        val adapter = ItemAdapter(context, data)
+        assertEquals("ItemAdapter is not the correct size", data.size, adapter.itemCount)
+    }
+
+}
